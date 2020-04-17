f2ad1e4be1b27fb9e08404445a9ad20c2de166a8
筱川 <suchuan.sc@alibaba-inc.com>
2020-04-17 12:02:26 +0800

    feat：存取 & 缓存时间、空间

--- src/LRU/index.js
+++ src/LRU/index.js
@@ -1,11 +1,124 @@
 class LRU {
-  constructor() {}
+  size = 100;
+  timeout = 10000;
+  hasSetUp = false;
+  cache = new Map();
+  queue = [];
+  timer = {}; // 定时器，过期删除数据
 
-  get = () => {}
+  get = (key) => {
+    if (!this.hasSetUp) this.hasSetUp = true;
 
-  set = () => {}
+    const value = this.cache.get(key);
+    if (value && value.value) {
+      return {
+        key,
+        value,
+      };
+    } else {
+      return {
+        mes: "未找到对应缓存",
+      };
+    }
+  }
 
-  setSize = () => {}
+  put = (key, value) => {
+    if (!key || !value) return null;
+    if (!this.hasSetUp) this.hasSetUp = true;
 
-  setTimeout = () => {}
+    if (!this.cache.has(key)) {
+      if (this.queue.length < this.size) {
+        this.queue.push({
+          key: key,
+          value: value,
+        });
+        this.cache.set(key, {
+          value: value,
+          index: this.queue.length - 1, // 当前数据在队列中的索引
+        });
+
+        this.timer[key] = setTimeout(() => this.willDeleteData(key), this.timeout);
+      } else {
+        const deletedItem = this.queue.shift();
+        clearTimeout(this.timer[deletedItem.key]);
+        this.cache.delete(deletedItem.key);
+        this.queue.forEach((itemValue,itemKey) => {
+          this.cache.set(itemKey, {
+            value: itemValue.value,
+            index: itemValue.index - 1,
+          });
+        });
+
+        this.queue.push({
+          key: key,
+          value: value,
+        });
+        this.cache.set(key, {
+          value: value,
+          index: this.queue.length - 1,
+        });
+
+        this.timer[key] = setTimeout(() => this.willDeleteData(key), this.timeout);
+      }
+    } else {
+      clearTimeout(this.timer[key]);
+      const index = this.cache.get(key).index;
+      const updatedItem = this.queue.splice(index, 1)[0];
+      updatedItem.value = value;
+
+      this.queue.push(updatedItem);
+      this.cache.forEach((itemValue, itemKey) => {
+        if (key === itemKey) {
+          this.cache.set(itemKey, {
+            value: value,
+            index: this.queue.length - 1,
+          });
+        } else if (itemValue.index > index) {
+          this.cache.set(itemKey, {
+            value: itemValue.value,
+            index: itemValue.index - 1,
+          });
+        }
+      });
+
+      this.timer[key] = setTimeout(() => this.willDeleteData(key), this.timeout);
+    }
+  }
+
+  setSize = (size) => {
+    if (this.hasSetUp) return null;
+
+    this.size = Number(size);
+    this.hasSetUp = true;
+  }
+
+  // 设置缓存过期时间
+  setTimeout = (time) => {
+    if (this.hasSetUp) return null;
+
+    this.timeout = time * 1000;
+    this.hasSetUp = true;
+  }
+
+  willDeleteData = (key) => {
+    let index = -1;
+    this.queue.forEach((item, i) => {
+      if (item.key === key) {
+        index = i;
+      }
+    });
+
+    if (index < 0) return;
+
+    this.queue.splice(index, 1);
+    this.cache.delete(key);
+    this.cache.forEach((itemValue, itemKey) => {
+      if (itemValue.index > index) {
+        this.cache.set(itemKey, {
+          value: itemValue.value,
+          index: itemValue.index - 1,
+        });
+      }
+    });
+  }
 }
