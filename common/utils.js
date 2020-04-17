435c3924a793cf6a8fbbafd1021b82a17cb13a02
筱川 <suchuan.sc@alibaba-inc.com>
2020-04-17 16:24:13 +0800

    feat：计算缓存占据内容空间

---
+++ common/utils.js
@@ -0,0 +1,54 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-17 14:45:06
+ * @LastEditTime: 2020-04-17 16:23:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /friday/common/utils.js
+ */
+const ECMA_SIZES = {
+  STRING: 2,
+  BOOLEAN: 4,
+  NUMBER: 8,
+}
+
+export function sizeof(obj) {
+  let objType = typeof obj;
+
+  switch (objType) {
+    case "string":
+      return ECMA_SIZES.STRING * obj.length;
+    case "boolean":
+      return ECMA_SIZES.BOOLEAN;
+    case "number":
+      return ECMA_SIZES.NUMBER;
+    case "symbol":
+      return;
+    case "object":
+      if (Array.isArray(obj)) {
+        return obj.map((item) => sizeof(item)).reduce((acc, curr) => acc + curr);
+      } else {
+        return sizeofObject(obj);
+      }
+    default:
+      return 0;
+  }
+}
+
+export function sizeofObject(obj) {
+  if (!obj) return 0;
+  const keys = [];
+  const bytes = 0;
+
+  for (let key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      keys.push(key);
+    }
+  }
+  keys.forEach(key => {
+    bytes += sizeof(key);
+    bytes += sizeof(obj[key]);
+  });
+
+  return bytes;
+}
